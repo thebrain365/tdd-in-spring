805c23090d9f34825659f83172663a1d0457fbf3
Muano <muano.m365@gmail.com>
2024-02-04 13:54:58 +0200

    created a simple controller and a controller test which is passing

--- posts/src/main/java/com/muano/brainson/post/PostController.java
+++ posts/src/main/java/com/muano/brainson/post/PostController.java
@@ -1,5 +1,17 @@
 package com.muano.brainson.post;
 
-public class PostController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
+class PostController {
+
+    @GetMapping("")
+    List<Post> findAll() {
+        return null;
+    }
 }
--- posts/src/test/java/com/muano/brainson/post/PostControllerTest.java
+++ posts/src/test/java/com/muano/brainson/post/PostControllerTest.java
@@ -1,7 +1,7 @@
 package com.muano.brainson.post;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,11 +10,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(PostController.class)
 @AutoConfigureMockMvc
-class PostControllerTest {
+public class PostControllerTest {
 
     @Autowired
     MockMvc mockMvc;
@@ -27,6 +28,12 @@ class PostControllerTest {
                 new Post(1, 1, "Test post 1", "Hello, World", null),
                 new Post(2, 1, "Test post 2", "My name is Muano", null)
         );
+    }
+
+    @Test
+    public void shouldReturnAllPosts() throws Exception {
+        mockMvc.perform(get("/api/posts"))
+                .andExpect(status().isOk());
     }
 }
 
